771678a29a14c38482121ba1a9b36b566a964470
Garret Karns <garretcmk@gmail.com>
2024-11-07 12:06:16 -0500

    finally added testing

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -11,3 +11,12 @@ T half(T value) {
 int half(int value) {
     return static_cast<int>(std::round(value / 2.0));
 }
+
+int main() {
+    // testing
+    std::cout << "half(10) = " << half(10) << std::endl;
+    std::cout << "half(5.0) = " << half(5.0) << std::endl;
+    std::cout << "half(5.5f) = " << half(5.5f) << std::endl;
+
+    return 0;
+}
